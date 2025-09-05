e600474365b078ef72f4c3c45a2d9e1bccf1aa32
Bala Anandhan <146362697+Balaanand26@users.noreply.github.com>
2025-09-05 22:37:15 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:3000/login", {
+    const response = await fetch("https://ps-form-website-backend.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
